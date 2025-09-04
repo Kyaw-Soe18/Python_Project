fb40b3988724453e6ce0525a466748f48169353b
LaMinPyaePyaeZaw <laminpyaepyaezaw18@gmail.com>
2025-09-04 22:41:34 +0630

    Fix static files config for Render

--- ams/settings.py
+++ ams/settings.py
@@ -122,7 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+# Where collected static files will be stored (needed for Render)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = (
